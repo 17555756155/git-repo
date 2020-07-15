400f582d01f8b128976df9b157b71046e951322b
admin <it1@tom.com>
2020-07-15 10:18:52 +0800

    修改了代码

--- Microservice_WFW_demo_01/service_consumer/src/main/java/com/chengeng/ServiceConsumerApplication.java
+++ Microservice_WFW_demo_01/service_consumer/src/main/java/com/chengeng/ServiceConsumerApplication.java
@@ -1,14 +1,8 @@
 package com.chengeng;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /*@SpringBootApplication
 @EnableDiscoveryClient
@@ -26,5 +20,4 @@ public class ServiceConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceConsumerApplication.class, args);
     }
-
 }
